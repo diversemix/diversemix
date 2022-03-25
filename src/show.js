a1097ba0e8cfa0862d135acdab791e82f28f2d70
diversemix <diversemix@gmail.com>
2022-03-25 17:05:09 +0000

    fix path to templates

--- src/show.js
+++ src/show.js
@@ -1,11 +1,16 @@
 import fs from 'fs';
+import path from 'path';
 import chalk from 'chalk';
 
 export default function show(template) {
     try {
-        process.stdout.write(fs.readFileSync(`templates/show/${template}`));
+        const templatePath = path.resolve(
+            new URL(import.meta.url).pathname,
+            '../../templates'
+        );
+        process.stdout.write(fs.readFileSync(`${templatePath}/show/${template}`));
     } catch(err) {
-        // console.error(err);
+        console.error(err);
         console.error(`Could not find template: ${chalk.red.bold(template)}`);
     }   
 }
